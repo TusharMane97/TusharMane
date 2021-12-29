251d50d1de50b6e4671a53a67a0ee9a54c0721c9
TusharMane97 <95534191+TusharMane97@users.noreply.github.com>
2021-12-29 14:15:19 +0530

    Create CodePoint.java

---
+++ StringPrograms/CodePoint.java
@@ -0,0 +1,12 @@
+package stringPrograms;
+
+public class CodePoint {
+	
+	public static void main(String[] args) {
+
+		String str = new String("Tushar");
+	
+		System.out.println(str.codePointAt(1));
+	}
+
+}
